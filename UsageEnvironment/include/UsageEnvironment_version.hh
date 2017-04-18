22852e014995d1ffc49d56c769e011bba2580301
k0zmo <k0zmo@outlook.com>
2017-04-18 23:03:25 +0200

    2017.04.10

--- UsageEnvironment/include/UsageEnvironment_version.hh
+++ UsageEnvironment/include/UsageEnvironment_version.hh
@@ -1,10 +1,10 @@
 // Version information for the "UsageEnvironment" library
-// Copyright (c) 1996-2016 Live Networks, Inc.  All rights reserved.
+// Copyright (c) 1996-2017 Live Networks, Inc.  All rights reserved.
 
 #ifndef _USAGEENVIRONMENT_VERSION_HH
 #define _USAGEENVIRONMENT_VERSION_HH
 
-#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2016.11.28"
-#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1480291200
+#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2017.04.10"
+#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1491782400
 
 #endif
--- liveMedia/include/liveMedia_version.hh
+++ liveMedia/include/liveMedia_version.hh
@@ -1,10 +1,10 @@
 // Version information for the "liveMedia" library
-// Copyright (c) 1996-2016 Live Networks, Inc.  All rights reserved.
+// Copyright (c) 1996-2017 Live Networks, Inc.  All rights reserved.
 
 #ifndef _LIVEMEDIA_VERSION_HH
 #define _LIVEMEDIA_VERSION_HH
 
-#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2016.11.28"
-#define LIVEMEDIA_LIBRARY_VERSION_INT		1480291200
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2017.04.10"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1491782400
 
 #endif
