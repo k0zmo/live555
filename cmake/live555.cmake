547cc17a038ae90c0dd913fae40c45f6fe4a6975
k0zmo <k0zmo@outlook.com>
2018-05-14 00:09:51 +0200

    add install target

--- cmake/live555.cmake
+++ cmake/live555.cmake
@@ -1,28 +1,32 @@
-function(target_live555_compile_options _target)
+function(live555_compile_options _target)
     if(WIN32)
-        target_compile_definitions(${_target} PUBLIC NOMINMAX
-                                                     _WIN32_WINNT=0x0601
-                                                     LOCALE_NOT_USED
-                                                     SOCKLEN_T=int)
+        target_compile_definitions(${_target} 
+            PUBLIC
+                LOCALE_NOT_USED
+                SOCKLEN_T=int
+        )
+        if(MSVC)
+            # Turn off the noise
+            target_compile_definitions(${_target}
+                PRIVATE
+                    _CRT_SECURE_NO_WARNINGS
+                    _WINSOCK_DEPRECATED_NO_WARNINGS
+            )
+        endif()
     else()
-        target_compile_definitions(${_target} PUBLIC BSD=1
-                                                     SOCKLEN_T=socklen_t)
+        target_compile_definitions(${_target}
+            PUBLIC
+                BSD=1
+                SOCKLEN_T=socklen_t
+        )
     endif()
+endfunction(live555_compile_options)
 
-    if(MSVC)
-        target_compile_options(${_target} PRIVATE /W3)
-        target_compile_definitions(${_target} PRIVATE _CRT_SECURE_NO_WARNINGS
-                                                      _WINSOCK_DEPRECATED_NO_WARNINGS)
-    else()
-        target_compile_options(${_target} PRIVATE -Wall)
-    endif()
-endfunction()
-
-function(add_test_executable _target)
+function(live555_add_test_executable _target)
     add_executable(${_target} ${ARGN})
-    target_live555_compile_options(${_target})
+    live555_compile_options(${_target})
     target_link_libraries(${_target} liveMedia BasicUsageEnvironment)
     if(TARGET EpollTaskScheduler)
         target_link_libraries(${_target} EpollTaskScheduler)
     endif()
-endfunction()
+endfunction(live555_add_test_executable)
