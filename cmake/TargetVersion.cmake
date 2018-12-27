c17b065e975e32073500ef34ddd713da928588b5
k0zmo <k0zmo@outlook.com>
2018-12-27 21:36:20 +0100

    Add interface target for propagation of compile options

--- cmake/TargetVersion.cmake
+++ cmake/TargetVersion.cmake
@@ -1,27 +1,3 @@
-function(live555_compile_options _target)
-    if(WIN32)
-        target_compile_definitions(${_target} 
-            PUBLIC
-                LOCALE_NOT_USED
-                SOCKLEN_T=int
-        )
-        if(MSVC)
-            # Turn off the noise
-            target_compile_definitions(${_target}
-                PRIVATE
-                    _CRT_SECURE_NO_WARNINGS
-                    _WINSOCK_DEPRECATED_NO_WARNINGS
-            )
-        endif()
-    else()
-        target_compile_definitions(${_target}
-            PUBLIC
-                BSD=1
-                SOCKLEN_T=socklen_t
-        )
-    endif()
-endfunction()
-
 function(__live555_target_version_impl _target _current _revision _age)
     math(EXPR version_major "${_current} - ${_age}")
     set_target_properties(${_target}
@@ -64,11 +40,3 @@ function(live555_target_version _target)
     )
 endfunction()
 
-function(live555_add_test_executable _target)
-    add_executable(${_target} ${ARGN})
-    live555_compile_options(${_target})
-    target_link_libraries(${_target} liveMedia BasicUsageEnvironment)
-    if(TARGET EpollTaskScheduler)
-        target_link_libraries(${_target} EpollTaskScheduler)
-    endif()
-endfunction()
