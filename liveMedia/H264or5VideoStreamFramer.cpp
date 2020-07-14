ffb28df58f6cfe699f87a329a270d838b3d624ca
k0zmo <k0zmo@outlook.com>
2020-07-15 00:03:20 +0200

    2020.07.09

--- liveMedia/H264or5VideoStreamFramer.cpp
+++ liveMedia/H264or5VideoStreamFramer.cpp
@@ -21,6 +21,7 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #include "H264or5VideoStreamFramer.hh"
 #include "MPEGVideoStreamParser.hh"
 #include "BitVector.hh"
+#include <GroupsockHelper.hh> // for "gettimeofday()"
 
 ////////// H264or5VideoStreamParser definition //////////
 
